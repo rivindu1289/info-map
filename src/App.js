7d04217a258636cd5dd2c366d0a27640abdea546
rivindu1289 <rivindu@outlook.com>
2020-06-30 12:34:16 -0400

    added am4charts imports and basic App class

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+
+am4core.useTheme(am4themes_animated);
+
+class App extends Component {
+  componentDidMount() {
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+    // ... chart code goes here ...
+
+    this.chart = chart;
+  }
+
+  componentWillUnmount() {
+    if (this.chart) {
+      this.chart.dispose();
+    }
+  }
+
+  render() {
+    return (
+      <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+    );
+  }
 }
 
 export default App;
