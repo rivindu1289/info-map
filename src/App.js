54a625166207ff34a7b987087f240607cbcfbaac
rivindu1289 <rivindu@outlook.com>
2020-06-30 13:00:09 -0400

    added world map with hover

--- src/App.js
+++ src/App.js
@@ -5,16 +5,29 @@ import './App.css';
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import * as am4maps from "@amcharts/amcharts4/maps";
+import am4geodata_worldLow from "@amcharts/amcharts4-geodata/worldLow";
 
 am4core.useTheme(am4themes_animated);
 
 class App extends Component {
   componentDidMount() {
-    let chart = am4core.create("chartdiv", am4charts.XYChart);
+    let map = am4core.create("chartdiv", am4charts.XYChart);
+    map.geodata = am4geodata_worldLow;
 
-    // ... chart code goes here ...
+    map.projection = new am4maps.projections.Miller();
+    let polygonSeries = map.series.push(new am4maps.MapPolygonSeries());
+    polygonSeries.useGeodata = true;
 
-    this.chart = chart;
+    // Configure series
+    let polygonTemplate = polygonSeries.mapPolygons.template;
+    polygonTemplate.fill = am4core.color("#74B266");
+
+    // Create hover state and set alternative fill color
+    let hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = am4core.color("#367B25");
+
+    this.chart = map;
   }
 
   componentWillUnmount() {
